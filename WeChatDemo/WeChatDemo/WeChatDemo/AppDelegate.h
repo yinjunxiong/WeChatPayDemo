ffade5036e62d4ee216e8a591c1fb1e5ec4183f4
yinjunxiong <yinjunxiong@yinjunxiongdeMacBook-Pro.local>
2015-07-07 09:47:17 +0800

    “first”

---
+++ WeChatDemo/WeChatDemo/WeChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatDemo
+//
+//  Created by 尹俊雄 on 15/7/2.
+//  Copyright (c) 2015年 yjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WXApi.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate,WXApiDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatDemo/WeChatDemo/WeChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatDemo
+//
+//  Created by 尹俊雄 on 15/7/2.
+//  Copyright (c) 2015年 yjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
